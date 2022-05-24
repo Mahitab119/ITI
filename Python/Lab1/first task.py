74fdc35e75ea63668e28842529a20d5583137498
Mahitab Mohsen <80608135+Mahitab119@users.noreply.github.com>
2022-05-24 16:22:02 +0200

    PYTHON LAB1

---
+++ Python/Lab1/first task.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+import math
+
+# #problem 1
+# def get_distance(x1,y1,x2,y2):
+#     d=math.sqrt(pow(x2-x1,2)+pow(y2-y1,2))
+#     return d
+
+# result=get_distance(2,3,5,6)
+# print(float(result))
+
+# #problem 2
+# def filter_numbers(list=[1,2,3,2,5,2]):
+#     myset=set(list)
+#     return myset
+
+# result=filter_numbers()
+# print(result)
